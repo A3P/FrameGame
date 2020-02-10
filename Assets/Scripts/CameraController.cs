cb016721993aabf8c19fc4e8be08e678b6ed0353
Amir Afshar <athreep@yahoo.com>
2020-02-09 20:10:50 -0800

    Camera positions itself in between the two players

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player1;
+
+    public GameObject player2;
+
+    private Camera cam;
+
+    private Vector3 zOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        zOffset = transform.position;
+        cam = gameObject.GetComponent<Camera>();
+    }
+
+    // LateUpdate is used here since it will also include changes made in the most recent update
+    void LateUpdate()
+    {
+        transform.position = zOffset + (player1.transform.position + player2.transform.position)/2;
+        float distance = Vector2.Distance(player1.transform.position, player2.transform.position);
+        cam.orthographicSize = distance/2;
+    }
+}
