5905ad34377adc3196da0759d95224af45c3b261
Amir Afshar <athreep@yahoo.com>
2020-03-03 11:30:38 -0800

    Attach script to percent tracker for controlling its value

---
+++ Assets/Scripts/PlayerPercent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPercent : MonoBehaviour
+{
+    private Text percentText;
+    private float percent;
+    public int playerNumber; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        percentText = gameObject.GetComponent<Text>();
+        percent = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void addPercent(float damage)
+    {
+        percent += damage;
+        percentText.text = "Player " + playerNumber + "\n" + percent + "%";
+    }
+}
